aabb51657a246bb37174c0f439f53ff2f95e20b3
minemidnight <minemidnight@users.noreply.github.com>
2019-01-13 01:50:51 -0500

    Fix small error and fix env var name

--- src/shardManager.js
+++ src/shardManager.js
@@ -2,14 +2,14 @@ const k8s = require("./kubernetes");
 const Redis = require("ioredis");
 const redis = new Redis({
 	port: +process.env.REDIS_PORT,
-	host: process.env.HOST,
+	host: process.env.REDIS_HOST,
 	family: 4,
 	db: +process.env.REDIS_DATABASE
 });
 
 module.exports = bucket => ({
 	async checkShards() {
-		const { url, shardCount: shards } = await bucket.request("GetGateway");
+		const { url, shards: shardCount } = await bucket.request("GetGateway");
 
 		const shards = Array.from(shardCount, (value, i) => i);
 	}
