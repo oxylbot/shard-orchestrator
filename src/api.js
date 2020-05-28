cc70aef9a8909a4cd68b9730f95fbf7ec6912de9
minemidnight <minemidnightmc@gmail.com>
2020-05-28 18:01:56 -0400

    Add support for max concurrency

--- src/api.js
+++ src/api.js
@@ -12,10 +12,10 @@ app.set("env", process.env.NODE_ENV);
 app.use(express.json());
 
 async function reshardCheck(options = {}) {
-	const { url, shards: shardCount } = await app.locals.bucket.request("getGateway");
+	const { url, shards: shardCount, maxConcurrency } = await app.locals.bucket.request("getGateway");
 
-	if(options.force || Math.round(app.locals.shardCount * options.scaleAt) <= shardCount) {
-		reshard({ url, shardCount });
+	if(options.force || Math.round(app.locals.sharding.shardCount * options.scaleAt) <= shardCount) {
+		reshard({ url, shardCount, maxConcurrency });
 	}
 }
 
@@ -24,15 +24,6 @@ module.exports = async (redis, bucket) => {
 	app.locals.redis = redis;
 	k8s = await kubernetes();
 
-	app.locals.sharding = {
-		shardCount: 0,
-		shardsAvailable: [],
-		gatewayURL: null,
-		waiting: new Map(),
-		lastStart: -1,
-		available: false
-	};
-
 	await reshardCheck({ force: true });
 };
 
@@ -40,7 +31,7 @@ setInterval(async () => {
 	await reshardCheck({ scaleAt: 1.25 });
 }, (1000 * 60) * 30);
 
-async function reshard({ url, shardCount }) {
+async function reshard({ url, shardCount, maxConcurrency }) {
 	if(app.locals.sharding.shardCount !== 0) await k8s.scale(0);
 	const replicas = Math.max(Math.ceil(shardCount / +process.env.SHARDS_PER_SHARDER), 1);
 	await k8s.scale(replicas);
@@ -51,7 +42,8 @@ async function reshard({ url, shardCount }) {
 		gatewayURL: url,
 		waiting: new Map(),
 		lastStart: -1,
-		available: true
+		available: maxConcurrency,
+		maxConcurrency
 	};
 
 	console.log("Resharded with", shardCount, "shards at", replicas, "replicas");
@@ -63,7 +55,7 @@ app.get("/shards", async (req, res) => {
 	const sharding = req.app.locals.sharding;
 	console.log(req.query.hostname, "is trying to get shards");
 	console.log("Sharding", sharding);
-	if(sharding.available) {
+	if(sharding.available > 0) {
 		console.log("Shards are available");
 		const cachedShards = await req.app.locals.redis.exists(`pod:${req.query.hostname}`);
 		if(sharding.shardsAvailable.length === 0 && !cachedShards) {
@@ -77,7 +69,7 @@ app.get("/shards", async (req, res) => {
 
 		console.log("Shards to give", shards);
 		sharding.lastStart = Date.now();
-		sharding.available = false;
+		sharding.available -= 1;
 
 		res.status(200).json({
 			shard_count: sharding.shardCount,
@@ -87,22 +79,18 @@ app.get("/shards", async (req, res) => {
 
 		sharding.waiting.delete(req.query.hostname);
 		await req.app.locals.redis.set(`pod:${req.query.hostname}`, JSON.stringify(shards));
+		setTimeout(() => sharding.available += 1, 5000);
 	} else {
 		sharding.waiting.set(req.query.hostname, true);
 		console.log("Time to wait!");
+
+		const extraTime = ((5000 / sharding.maxConcurrency) * +process.env.SHARDS_PER_SHARDER) *
+							(sharding.waiting.size + 1);
 		res.status(429).json({
-			retry_at: sharding.lastStart + ((6000 * +process.env.SHARDS_PER_SHARDER) * (sharding.waiting.size + 1)),
+			retry_at: sharding.lastStart + extraTime,
 			waiting: sharding.waiting.size
 		});
 	}
-});
-
-app.put("/finished", async (req, res) => {
-	console.log(req.body.hostname, "is finished");
-	const sharding = req.app.locals.sharding;
-	sharding.available = true;
-
-	res.status(204).end();
 });
 
 app.all("*", (req, res) => {
