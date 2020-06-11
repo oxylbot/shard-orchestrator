31912a5fb3a215e8e645e21937ed04aebf150e4c
minemidnight <minemidnightmc@gmail.com>
2020-06-10 20:36:08 -0400

    Add bucket-proto submodule

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const BucketClient = require("./BucketClient");
 const bucketClient = new BucketClient();
 
 async function init() {
-	const rpcProto = await protobuf.load(path.resolve(__dirname, "..", "protobuf", "rpcWrapper.proto"));
-	const discordProto = await protobuf.load(path.resolve(__dirname, "..", "protobuf", "discordapi", "service.proto"));
+	const rpcProto = await protobuf.load(path.resolve(__dirname, "..", "bucket-proto", "rpcWrapper.proto"));
+	const discordProto = await protobuf.load(path.resolve(__dirname, "..", "bucket-proto", "service.proto"));
 	bucketClient.start({
 		discord: discordProto,
 		rpc: rpcProto
