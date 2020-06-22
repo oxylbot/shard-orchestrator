2d9c5443879d7564c0cb78b6e0572794c28ec8b5
minemidnight <minemidnightmc@gmail.com>
2020-06-22 18:10:31 -0400

    Fix require stack

--- src/rest.js
+++ src/rest.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const expressWinston = require("express-winston");
 const kubernetes = require("./kubernetes");
-const logger = require("../logger");
+const logger = require("./logger");
 const superagent = require("superagent");
 let k8s;
 
